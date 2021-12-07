b72f45d5a5a38e89bbcf35191351b51fb2b6fea0
Alejandro González <thytanium@gmail.com>
2021-12-06 23:16:54 -0300

    Export useOutsideClick

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as useLocalStorage } from "./hooks/useLocalStorage";
 export { default as useMounted } from "./hooks/useMounted";
+export { default as useOutsideClick } from "./hooks/useOutsideClick";
 export { default as usePrevious } from "./hooks/usePrevious";
